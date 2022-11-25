34fa9ab15662415e5c822774096a20da666b9ac4
yilin18 <yilin18@illinois.edu>
2022-11-25 14:59:17 -0600

    First Demo

---
+++ src/router/navigator/navigator.component.jsx
@@ -0,0 +1,20 @@
+import { Link, Outlet } from "react-router-dom"
+import { Fragment } from "react"
+import './navigation.styles.scss'
+
+const Navigator = ()=>{
+    return (
+        <Fragment>
+            <div className="navigation">
+                <div className="nav-links-container"></div>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="about">About</Link>
+                </div>
+            </div>
+            <Outlet></Outlet>
+        </Fragment>
+    )
+}
+
+export default Navigator
